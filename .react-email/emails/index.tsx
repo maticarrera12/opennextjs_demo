8823e9ce961fe1ecac37280517137c330d4554fb
MatiCarrera <mcarreradev12@gmail.com>
2025-11-03 12:56:26 -0300

    added export for deletedaccount

--- .react-email/emails/index.tsx
+++ .react-email/emails/index.tsx
@@ -2,3 +2,4 @@ export { default as WelcomeEmail } from "../../emails/welcome-email";
 export { default as PasswordResetEmail } from "../../emails/password-reset-email";
 export { default as WaitlistWelcomeEmail } from "../../emails/waitlist-welcome-email";
 export { default as ChangeEmailVerification } from "../../emails/change-email-verification";
+export { default as DeleteAccountVerification } from "../../emails/delete-account-verification";
